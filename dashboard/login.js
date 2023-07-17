5f3636e17100c40f8254a38d07c9b905b957d84d
HIVERRO <hiverro0@gmail.com>
2023-07-17 14:25:09 +0200

    main

---
+++ dashboard/login.js
@@ -0,0 +1,85 @@
+// Dodaj poniższy kod do obszaru <script> w pliku login.js lub pomiń istniejący kod.
+
+// Konfiguracja
+const clientID = '848629390577500211';
+const redirectURI = 'http://127.0.0.1:5500/dashboard/callback.html';
+const discordAPI = 'https://discord.com/api/v10';
+
+// Sprawdź, czy użytkownik jest już zalogowany
+checkLoginStatus();
+
+// Funkcja sprawdzająca status logowania
+function checkLoginStatus() {
+  const token = localStorage.getItem('accessToken');
+
+  if (token) {
+    // Wykonaj żądanie, aby pobrać dane użytkownika
+    axios.get(`${discordAPI}/users/@me`, {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      }
+    })
+    .then(response => {
+      // Użytkownik jest zalogowany
+      const username = response.data.username;
+      const discriminator = response.data.discriminator;
+      showDashboard(`${username}#${discriminator}`);
+    })
+    .catch(error => {
+      // Wystąpił błąd lub token jest nieważny
+      console.error('Błąd podczas pobierania danych użytkownika:', error);
+      showLogin();
+    });
+  } else {
+    // Użytkownik nie jest zalogowany
+    showLogin();
+  }
+}
+
+// Funkcja wyświetlająca panel bota po zalogowaniu
+function showDashboard(username) {
+  const loginElement = document.getElementById('login');
+  const dashboardElement = document.getElementById('dashboard');
+
+  if (loginElement && dashboardElement) {
+    loginElement.style.display = 'none';
+    dashboardElement.style.display = 'block';
+    document.getElementById('username').textContent = username;
+  }
+}
+
+// Funkcja wyświetlająca formularz logowania
+function showLogin() {
+  const loginElement = document.getElementById('login');
+  const dashboardElement = document.getElementById('dashboard');
+
+  if (loginElement && dashboardElement) {
+    loginElement.style.display = 'block';
+    dashboardElement.style.display = 'none';
+  }
+}
+
+// Funkcja wylogowująca użytkownika
+function logout() {
+  localStorage.removeItem('accessToken');
+  showLogin();
+}
+
+// Funkcja odbierająca token dostępu z adresu URL
+function handleAccessToken() {
+  const params = new URLSearchParams(window.location.hash.substr(1));
+  const accessToken = params.get('access_token');
+
+  if (accessToken) {
+    // Zapisz token dostępu w localStorage
+    localStorage.setItem('accessToken', accessToken);
+
+    // Przeładuj stronę, aby usunąć token z adresu URL
+    window.location.replace(redirectURI);
+  } else {
+    showLogin();
+  }
+}
+
+// Wywołaj funkcję handleAccessToken przy załadowaniu strony
+handleAccessToken();
